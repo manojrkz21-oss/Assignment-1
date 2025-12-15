1da397778defb03600a29aab123872dc7e7edeb3
manojrkz21-oss <manojrkz21@gmail.com>
2025-12-16 03:08:53 +0530

    first project

---
+++ Assignment_1.py
@@ -0,0 +1,27 @@
+# input two numbers from the user
+
+a = float(input("Enter the First Numer: "))
+b = float(input("Enter the Second Numer: "))
+
+# Addition
+sum = a + b
+print("Addition: ", sum)
+
+# Substraction
+sub = a - b
+print("Substraction: ", sub)
+
+# Multiplication
+mul = a * b
+print("Multipliaction: ", mul)
+
+# Division
+div = a / b
+print("Division: ", div)
+
+# taking input from user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Greeting message using concatenation
+print("Hello,", first_name + " " + last_name + "! Welcome to the Python program.")
